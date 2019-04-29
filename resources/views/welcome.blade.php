851a27078b989dea63ec75b10240c1a70f761cb1
air <asilkhan-al@mail.ru>
2019-04-29 18:35:14 +0600

    nurbot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    NurBot
+                    SurBot
                 </div>
             </div>
         </div>
